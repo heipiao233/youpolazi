c1de13eeb0ebc9ff2cb2639f36fc4e431d3d5cb3
heipiao233 <heipiao233@outlook.com>
2022-07-18 16:23:45 +0800

    创造模式物品栏

--- src/main/java/net/heipiao/youpolazi/Items.java
+++ src/main/java/net/heipiao/youpolazi/Items.java
@@ -15,11 +15,11 @@ public final class Items {
         };
     };
     public static final DeferredRegister<Item> ITEMS=DeferredRegister.create(ForgeRegistries.ITEMS, "youpolazi");
-    public static final RegistryObject<Item> PEPPER=ITEMS.register("pepper", ()->new Item(new Item.Properties()));
-    public static final RegistryObject<Item> SESAME=ITEMS.register("sesame", ()->new Item(new Item.Properties()));
-    public static final RegistryObject<Item> ZANTHOXYLUM=ITEMS.register("zanthoxylum", ()->new Item(new Item.Properties()));
-    public static final RegistryObject<Item> EDIBLE_OIL_BOTTLE=ITEMS.register("edible_oil_bottle", ()->new Item(new Item.Properties().craftRemainder(net.minecraft.world.item.Items.GLASS_BOTTLE)));
-    public static final RegistryObject<Item> EDIBLE_OIL_BOTTLE_HOT=ITEMS.register("edible_oil_bottle_hot", ()->new Item(new Item.Properties().craftRemainder(net.minecraft.world.item.Items.GLASS_BOTTLE)));
-    public static final RegistryObject<Item> PAPRIKA=ITEMS.register("paprika", ()->new Item(new Item.Properties()));
-    public static final RegistryObject<Item> YOU_PO_LA_ZI_BOTTLE=ITEMS.register("you_po_la_zi_bottle", ()->new Item(new Item.Properties().craftRemainder(net.minecraft.world.item.Items.GLASS_BOTTLE)));
+    public static final RegistryObject<Item> PEPPER=ITEMS.register("pepper", ()->new Item(new Item.Properties().tab(TAB)));
+    public static final RegistryObject<Item> SESAME=ITEMS.register("sesame", ()->new Item(new Item.Properties().tab(TAB)));
+    public static final RegistryObject<Item> ZANTHOXYLUM=ITEMS.register("zanthoxylum", ()->new Item(new Item.Properties().tab(TAB)));
+    public static final RegistryObject<Item> EDIBLE_OIL_BOTTLE=ITEMS.register("edible_oil_bottle", ()->new Item(new Item.Properties().craftRemainder(net.minecraft.world.item.Items.GLASS_BOTTLE).tab(TAB)));
+    public static final RegistryObject<Item> EDIBLE_OIL_BOTTLE_HOT=ITEMS.register("edible_oil_bottle_hot", ()->new Item(new Item.Properties().craftRemainder(net.minecraft.world.item.Items.GLASS_BOTTLE).tab(TAB)));
+    public static final RegistryObject<Item> PAPRIKA=ITEMS.register("paprika", ()->new Item(new Item.Properties().tab(TAB)));
+    public static final RegistryObject<Item> YOU_PO_LA_ZI_BOTTLE=ITEMS.register("you_po_la_zi_bottle", ()->new Item(new Item.Properties().craftRemainder(net.minecraft.world.item.Items.GLASS_BOTTLE).tab(TAB)));
 }
